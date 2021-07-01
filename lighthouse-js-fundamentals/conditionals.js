68553b667112262a89d9db1bb4ebe38fa4288a40
Anh Tong <tongkimanh@yahoo.com>
2021-07-01 21:37:45 +0000

    added function whichSchool

--- lighthouse-js-fundamentals/conditionals.js
+++ lighthouse-js-fundamentals/conditionals.js
@@ -22,3 +22,13 @@ const age = 26;
 if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
 }
+
+function whichSchool(age) {
+  if (age < 13) {
+    return "Elementary School";
+  } else if (age <= 18){
+      return "Secondary School";
+    } else {
+        return "Lighthouse Labs";
+      }  
+}
